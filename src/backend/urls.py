c9678ed51609ec7924d49375cdff7a5f839acd6b
believeohiozua <believe.ohiozua@gmail.com>
2019-06-06 21:56:10 +0100

    modifications

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -7,10 +7,10 @@ from . import views
 
 urlpatterns = [
     path('', views.homePage, name='home'),
-    path('', views.workPage, name='work'),
-    path('', views.servicesPage, name='services'),
-    path('', views.contactPage, name='contact'),
-    path('', views.aboutPage, name='about'),
+    path('work/', views.workPage, name='work'),
+    path('services/', views.servicesPage, name='services'),
+    path('contact/', views.contactPage, name='contact'),
+    path('about/', views.aboutPage, name='about'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
